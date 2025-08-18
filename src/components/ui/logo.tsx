1687d42ef64aaba4e136fb579c514d2faddd6ae2
João Marcos <joaomarcosrodduarte@gmail.com>
2025-08-18 02:10:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -11,7 +11,7 @@ interface LogoProps {
 export function Logo({ width = 120, height = 32 }: LogoProps) {
   return (
     <Image
-      src="https://firebasestorage.googleapis.com/v0/b/qoro-iy1gs.appspot.com/o/qoro-logo.png?alt=media&token=425a8155-c543-4340-a1f7-e722c83675a8"
+      src="https://storage.googleapis.com/qoro-iy1gs.appspot.com/qoro-logo.png"
       alt="Qoro Logo"
       width={width}
       height={height}
