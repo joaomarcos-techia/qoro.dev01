6055b853e7f6d15e000b49b40b64a98045613198
João Marcos <joaomarcosrodduarte@gmail.com>
2025-08-18 02:26:34 +0000

    segue o link da imagem que substituira o nome Qoro: https://firebasestor

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -14,7 +14,7 @@ export function Logo({ height = 32 }: LogoProps) {
       style={{ width: `${width}px`, height: `${height}px` }}
     >
       <Image
-        src="/logo.svg"
+        src="https://firebasestorage.googleapis.com/v0/b/qoro-iy1gs.firebasestorage.app/o/logo_definitiva-removebg-preview.png?alt=media&token=a6de67b0-f90f-41a1-885f-a5ab5df437ac"
         alt="Qoro Logo"
         fill
         sizes={`${width}px`}
