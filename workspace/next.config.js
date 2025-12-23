b878357a6629fba6c294f6207adff96bf343c17c
João Marcos <joaomarcosrodduarte@gmail.com>
2025-12-23 01:09:33 +0000

    extensionHostWorker.esm.js:1  Failed to load resource: the server respon

---
+++ workspace/next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  distDir: 'build',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  experimental: {
+    webVitalsAttribution: ['CLS', 'LCP'],
+  }
+};
+
+module.exports = nextConfig;
