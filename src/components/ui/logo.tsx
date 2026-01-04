fb27d526d65bb33494867bb6f4809efc190eb12b
João Marcos <joaomarcosrodduarte@gmail.com>
2026-01-04 22:00:05 +0000

    a logo nao aparece
    qualificacao:1  GET https://6000-firebase-qoro2-1767

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -13,7 +13,7 @@ export function Logo({ height = 24, className }: LogoProps) {
   return (
     <div className={cn('relative', className)} style={{ height: `${height}px`, width: `${width}px`}}>
       <Image
-        src="https://raw.githubusercontent.com/joaomarcos-techia/qoro.dev01/main/public/logo_definitiva-removebg-preview.png"
+        src="https://firebasestorage.googleapis.com/v0/b/qoro-iy1gs.firebasestorage.app/o/logo_definitiva-removebg-preview.png?alt=media&token=a6de67b0-f90f-41a1-885f-a5ab5df437ac"
         alt="Qoro Logo"
         fill
         className="object-contain"
